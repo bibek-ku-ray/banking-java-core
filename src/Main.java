69691f1476a69331f779d5be43b24204f71bb47a
Bibek Ray <bibek.ray@deerwalk.edu.np>
2025-10-31 17:35:23 +0545

    Add domain model classes for banking system

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Welcome to Console Banking System");
+
+        boolean running = true;
+        while (running) {
+            System.out.println("""
+                    1. Open Account
+                    2. Deposit
+                    3. Withdraw
+                    4. Transfer
+                    5. Account statement
+                    6. List accounts
+                    7. Search accounts by name
+                    0. Exit
+                    """);
+
+            System.out.println("Choose option (Enter 0 to 7)");
+            System.out.print("Choose: ");
+            String choice = sc.nextLine().trim();
+            System.out.println("Choose: " + choice);
+
+            switch (choice) {
+                case "1" -> System.out.println("1");
+                case "2" -> System.out.println("2");
+                case "3" -> System.out.println("3");
+                case "4" -> System.out.println("4");
+                case "5" -> System.out.println("5");
+                case "6" -> System.out.println("6");
+                case "7" -> System.out.println("7");
+                case "0" -> {
+                    running = false;
+                    System.out.println("Program End 🎬");
+                }
+            }
         }
     }
+
+
 }
 
 /*
